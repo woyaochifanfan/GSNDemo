aa23c707627f055abc869f3fa51ca1d8489ababa
JimmyRothric <desertequal@outlook.com>
2016-12-08 20:13:44 +0800

    Version 1.0.1
    Add Accout Person

---
+++ src/GSNDemo/Account.java
@@ -0,0 +1,26 @@
+package GSNDemo;
+
+public class Account {
+
+	private int id;
+	private String password;
+	
+	public Account(int id, String password) {
+		this.id = id;
+		this.password = password;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
--- src/GSNDemo/Person.java
+++ src/GSNDemo/Person.java
@@ -1,5 +1,40 @@
 package GSNDemo;
 
 public class Person {
+	private Account account;
+	private String nickname;
+	private String comment;
+	
+	public Person(Account account, String nickname, String comment) {
+		this.account = account;
+		this.nickname = nickname;
+		this.comment = comment;
+	}
 
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	
+	
 }
